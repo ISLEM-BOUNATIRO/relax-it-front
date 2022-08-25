8a77d18d379207e0d136ccf6ecfd09ca5b9e504d
ISLEM-BOUNATIRO <bntr.islem@gmail.com>
2022-08-25 14:35:53 +0100

    first push

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const serveStatic = require('serve-static');
 const history = require('connect-history-api-fallback');
 const enforce = require("express-sslify");
-const app = express();
+const app = express(); 
 app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(serveStatic(__dirname + '/dist'));
 
