ac94eca0217d4e30da619aeb70b614cb9225b472
ISLEM-BOUNATIRO <110762618+ISLEM-BOUNATIRO@users.noreply.github.com>
2023-04-23 23:14:40 +0200

    Update .prettierrc.js

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,12 +1,12 @@
 module.exports = {
-  printWidth: 80, // 一行的最大长度
-  tabWidth: 2, // 缩进的空格数
-  useTabs: false, // 是否使用tab进行缩进
-  semi: false, // 句尾分号
-  singleQuote: true, // 单引号
-  trailingComma: 'none', // 句尾逗号
-  bracketSpacing: true, // key: value 之间的空格
-  arrowParens: 'always', // 箭头函数单个参数的情况是否省略括号
-  vueIndentScriptAndStyle: false, // vue script和style标签中是否缩进
-  endOfLine: 'lf' // 换行符
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  vueIndentScriptAndStyle: false,
+  endOfLine: 'lf'
 }
