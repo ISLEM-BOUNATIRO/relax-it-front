7568e98c33b4d43bdc1b51ffedb3ad487a4742cd
ISLEM-BOUNATIRO <110762618+ISLEM-BOUNATIRO@users.noreply.github.com>
2023-04-23 23:17:03 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      colors: {
+      colors: { 
         body: '#0c0c0c', // 可以在类中使用 bg-primary
         header: '#39393b' // 可以在类中使用 bg-secondary
       }
